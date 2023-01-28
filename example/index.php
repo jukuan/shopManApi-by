4b05e92d9196cb315fc26e21f1c28799e452f996
Juljan <y.misiukevich@gmail.com>
2023-01-28 18:24:42 +0300

    Initial commit: ShopManApi.php as wrapper for ymlparser

---
+++ example/index.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use LireinCore\YMLParser\Category;
+use LireinCore\YMLParser\Offer\AExtOffer;
+use ShopManApi\ShopManApi;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$handler = (new ShopManApi(42, '12demo89'))
+    ->parseYml('dealby.yml');
+
+/** @var DateTimeImmutable $date */
+$date = $handler->getDate();
+echo sprintf('Date: %s', $date->format('Y-m-d H:i:s')) . PHP_EOL;
+
+/** @var Category[] $categories */
+$categories = $handler->getCategories();
+echo sprintf('Categories count: %d', count($categories)) . PHP_EOL;
+
+/** @var AExtOffer[]|\Generator $offers */
+$offers = $handler->getOffers();
+if ($offers instanceof Generator) {
+    $offers = iterator_to_array($offers);
+}
+echo sprintf('Offers count: %d', count($offers)) . PHP_EOL;
+
+/** @var Exception $lastError */
+$lastError = $handler->getLastError();
+echo sprintf('Last error: %s', $lastError?->getMessage() ?? 'none') . PHP_EOL;
---
+++ src/ShopManApi.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShopManApi;
+
+use DateTimeImmutable;
+use Exception;
+use Generator;
+use LireinCore\YMLParser\Category;
+use LireinCore\YMLParser\Offer\AExtOffer;
+use LireinCore\YMLParser\Shop;
+use LireinCore\YMLParser\YML;
+use Psr\Log\LoggerInterface;
+
+class ShopManApi
+{
+    private const SM_API = 'https://api.shopmanager.by/d/api/';
+
+    private ?LoggerInterface $logger = null;
+
+    private ?YML $yml = null;
+
+    private ?Exception $lastError = null;
+
+    public function __construct(
+        private readonly int $shopId,
+        private readonly string $key
+    ) {
+    }
+
+    public function reset(): static
+    {
+        $this->yml = null;
+        $this->lastError = null;
+
+        return $this;
+    }
+
+    public function setLogger(?LoggerInterface $logger): ShopManApi
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    public function getLastError(): ?Exception
+    {
+        return $this->lastError;
+    }
+
+    public function parseYml(string $file): static
+    {
+        $url = self::SM_API . sprintf('%s?shopId=%d&key=%s', $file, $this->shopId, $this->key);
+
+        $this->yml = new YML();
+
+        try {
+            $this->yml->parse($url);
+        } catch (Exception $e) {
+            $this->yml = null;
+            $this->logException($e);
+        }
+
+        return $this;
+    }
+
+    public function getDate(): ?DateTimeImmutable
+    {
+        $date = $this->yml?->getDate();
+
+        if (null === $date || '' === $date) {
+            return null;
+        }
+
+        try {
+            return new DateTimeImmutable($date);
+        } catch (Exception $e) {
+            $this->logException($e);
+        }
+
+        return null;
+    }
+
+    public function getShop(): ?Shop
+    {
+        $shop = $this->yml?->getShop();
+
+        if (null === $shop || !$shop->isValid()) {
+            return null;
+        }
+
+        return $shop;
+    }
+
+    /**
+     * @return AExtOffer[]|Generator
+     */
+    public function getOffers(): iterable|Generator
+    {
+        try {
+            return $this->yml?->getOffers() ?? [];
+        } catch (Exception $e) {
+            $this->logException($e);
+        }
+    }
+
+    /**
+     * @return Category[]
+     */
+    public function getCategories(): iterable
+    {
+        $categories = $this->getShop()?->getCategories();
+
+        return $categories ?? [];
+    }
+
+    private function logException(Exception $e): void
+    {
+        $this->lastError = $e;
+
+        if (null === $this->logger) {
+            return;
+        }
+
+        $message = sprintf('ShopApiHandler: %s', $e->getMessage());
+        $this->logger->log('error', $message, [$e->getLine(), $e->getFile()]);
+    }
+}

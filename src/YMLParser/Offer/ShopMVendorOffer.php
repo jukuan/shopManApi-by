c64a78d2083c3263fe9b0cda55b286d448ebef3c
Juljan M. <y.misiukevich@gmail.com>
2024-02-19 16:30:18 +0300

    Add importer and serviceCenter fields to the Offer

--- src/YMLParser/Offer/ShopMVendorOffer.php
+++ src/YMLParser/Offer/ShopMVendorOffer.php
@@ -10,15 +10,22 @@ class ShopMVendorOffer extends VendorModelOffer
 {
     private int $quantityInStock = 0;
     private string $vendorName = '';
+    private string $importer = '';
+    private string $serviceCenter = '';
 
     public function addAttribute(array $attrNode): ShopMVendorOffer
     {
         $name = $attrNode['name'] ?? '';
+        $value = $attrNode['value'] ?? '';
 
         if ('quantity_in_stock' === $name) {
-            $this->quantityInStock = (int) ($attrNode['value'] ?? 0);
+            $this->quantityInStock = (int) $value;
         } else if ('vendor_name' === $name) {
-            $this->vendorName = $attrNode['value'] ?? '';
+            $this->vendorName = $value;
+        } else if ('importer' === $name) {
+            $this->importer = $value;
+        } else if ('service_center' === $name) {
+            $this->serviceCenter = $value;
         } else {
             parent::addAttribute($attrNode);
         }
@@ -34,5 +41,15 @@ class ShopMVendorOffer extends VendorModelOffer
     public function getVendorName(): string
     {
         return $this->vendorName;
+    }
+
+    public function getImporter(): string
+    {
+        return $this->importer;
+    }
+
+    public function getServiceCenter(): string
+    {
+        return $this->serviceCenter;
     }
 }
